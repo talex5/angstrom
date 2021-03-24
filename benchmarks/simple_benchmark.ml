f7644bfe6a6d818bca77b17cceaac7c172e15145
Thomas Leonard <talex5@gmail.com>
2021-03-24 09:43:41 +0000

    Improve benchmark
    On my machine:
    
        dune exec -- ./benchmarks/simple_benchmark.exe
        Name        Time  MinWrds  MajWrds  MinCol  MajCol
        http     11.18ms    4640k    50471      19       1
        count    44.25ms   55583k     1499     216       3

--- benchmarks/simple_benchmark.ml
+++ benchmarks/simple_benchmark.ml
@@ -11,16 +11,41 @@ let read file =
   Unix.close fd;
   Bigstringaf.of_string (Bytes.to_string buf) ~off:0 ~len:size
 
-let () =
-  let http_get = read "benchmarks/data/http-requests.txt.100" in
+let bench name n_iters p data =
+  let stat0 = Gc.stat () in
   let t0 = Unix.gettimeofday () in
-  let n_iters = 100 in
-  let p = Angstrom.skip_many RFC2616.request in
   for _ = 1 to n_iters do
-    match Angstrom.(parse_bigstring ~consume:All) p http_get with
+    match Angstrom.(parse_bigstring ~consume:All) p data with
     | Ok _ -> ()
     | Error err -> failwith err
   done;
   let t1 = Unix.gettimeofday () in
+  let stat1 = Gc.stat () in
   let time = t1 -. t0 in
-  Printf.printf "Took %.2fms per iteration\n" (1000. *. time /. float n_iters)
+  let n_iters = float n_iters in
+  let minor_words = (stat1.minor_words -. stat0.minor_words) /. n_iters in
+  let major_words = (stat1.major_words -. stat0.major_words) /. n_iters in
+  let minor_collections = float (stat1.minor_collections - stat0.minor_collections) /. n_iters in
+  let major_collections = float (stat1.major_collections - stat0.major_collections) /. n_iters in
+  Printf.printf "%-7s %6.2fms %7.0fk %8.0f %7.0f %7.0f\n"
+    name
+    (1000. *. time /. n_iters)
+    (minor_words/.1000.)
+    major_words
+    minor_collections
+    major_collections
+
+let chars =
+  Angstrom.(fix (fun p ->
+      any_char >>= fun _ ->
+      at_end_of_input >>= function
+      | true -> return ()
+      | false -> p
+    ))
+
+let () =
+  let http_get = read "benchmarks/data/http-requests.txt.100" in
+  let parse_http = Angstrom.skip_many RFC2616.request in
+  Printf.printf "Name        Time  MinWrds  MajWrds  MinCol  MajCol\n";
+  bench "http"  100 parse_http http_get;
+  bench "count" 10  chars http_get
